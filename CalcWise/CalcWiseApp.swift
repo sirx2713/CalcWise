932bce22df3a2e3e2476a139c31288a387b912cd
iSAD <bradx2713@gmail.com>
2024-08-23 15:44:48 +0200

    Initial Commit

---
+++ CalcWise/CalcWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcWiseApp.swift
+//  CalcWise
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/08/23.
+//
+
+import SwiftUI
+
+@main
+struct CalcWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalcWise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CalcWise
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
