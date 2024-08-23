4fd26d2d950e92877523e45cc8e81e93f2126a14
iSAD <bradx2713@gmail.com>
2024-08-23 15:53:28 +0200

    CalcWise

---
+++ CalcWise/ConvertingView.swift
@@ -0,0 +1,18 @@
+//
+//  ConvertingView.swift
+//  CalcWise
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/08/23.
+//
+
+import SwiftUI
+
+struct ConvertingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ConvertingView()
+}
